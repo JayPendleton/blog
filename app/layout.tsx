3ef67b295f1e46df22745887bd0bd38bb8676fe4
Jay <jaypendleton07@gmail.com>
2026-06-19 17:54:59 -0400

    removed duplicate

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={manrope.className}>{children}
+      <body className={manrope.className}>
       {children}
       <Analytics/>
       </body>
